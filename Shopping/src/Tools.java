2b930066d469176fdbe2acff4fb8ac4f0194f1f9
Charles <846743326@qq.com>
2017-07-04 13:49:42 +0800

    delete2

--- Shopping/src/Tools.java
+++
@@ -1,9 +0,0 @@
-package com.Shop;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public interface Tools {
-	public Map getMap();//提供实例化并获得map的方法
-	public void printMapFun(Map map);//提供格式化打印map的方法
-}
